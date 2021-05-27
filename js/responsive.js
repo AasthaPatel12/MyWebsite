c4295b2b48932236c65891dcbdec5ee27e9fa5c9
AasthaPatel12 <20d170025@iitb.ac.in>
2021-05-27 14:26:00 +0530

    addition of counter

--- js/responsive.js
+++ js/responsive.js
@@ -3,44 +3,30 @@ navbar = document.querySelector('.navbar')
 rightNav = document.querySelector('.rightNav')
 navList = document.querySelector('.nav-list')
 
-
 burger.addEventListener('click', () => {
     navList.classList.toggle('v');
     rightNav.classList.toggle('v');
     navbar.classList.toggle('h-nav');
 })
-/*
-var slide = document.getElementById("slider");
-var btn1 = document.getElementById('btn1');
-var btn2 = document.getElementById('btn2');
-var btn3 = document.getElementById('btn3');
-var btn4 = document.getElementById('btn4');
-
-btn1.onclick = function () {
-    slide.style.transform = "translateX(0px)";
-    btn1.classList.add('active');
-    btn2.classList.remove('active');
-    btn3.classList.remove('active');
-    btn4.classList.remive('active');
-};
-btn2.onclick = function () {
-    slide.style.transform = "translateX(-100%)";
-    btn1.classList.remove('active');
-    btn2.classList.add('active');
-    btn3.classList.remove('active');
-    btn4.classList.remove('active');
-};
-btn3.onclick = function () {
-    slide.style.transform = "translateX(-200%)";
-    btn1.classList.remove('active');
-    btn2.classList.remove('active');
-    btn3.classList.add('active');
-    btn4.classList.remove('active');
-};
-btn4.onclick = function () {
-    slide.style.transform = "translateX(-300%)";
-    btn1.classList.remove('active');
-    btn2.classList.remove('active');
-    btn3.classList.remove('active');
-    btn4.classList.add('active');
-};*/
+var input = 0;
+var counter = document.querySelector('.counter')
+var box1 = document.getElementById('one')
+var box2 = document.getElementById('two')
+var box3 = document.getElementById('three')
+var box4 = document.getElementById('four')
+box2.innerHTML = '<p>'+input+'</p>';
+box4.addEventListener('click' , reset);
+function reset(e){
+    input = 0;
+    box2.innerHTML = '<p>'+input+'</p>';
+}
+box3.addEventListener('click' , add);
+function add(e){
+    input++;
+    box2.innerHTML = '<p>'+input+'<p>';
+}
+box1.addEventListener('click' , remove);
+function remove(e){
+    input--;
+    box2.innerHTML = '<p>'+input+'</p>';
+}
